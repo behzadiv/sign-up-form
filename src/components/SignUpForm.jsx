309b58ecc0b0b1476768ecff3863b2bdf28b5223
Behzad <saeedibehzad@gmail.com>
2021-11-03 10:39:36 +0330

    install formik and use initialValues and handleChange

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,31 +1,46 @@
 import { useState } from "react";
-import "../App.css"
+import "../App.css";
+import { useFormik } from "formik";
 const SignUpForm = () => {
-    const[user,setUser]=useState({name:"",email:"",password:""})
-    const changeHandler = (e)=>{
-        setUser({...user,[e.target.name]:e.target.value})
-    }
-    const submitHandler =(e)=>{
-        e.preventDefault()
-        console.log("ok");
-    }
-    return (  
-        <form onSubmit={submitHandler}>
-            <div>
-                <label>Name</label>
-                <input type="text" name="name" onChange={changeHandler} value={user.name}/>
-            </div>
-            <div>
-                <label>Email</label>
-                <input type="text" name="email" onChange={changeHandler} value={user.email}/>
-            </div>
-            <div>
-                <label>Password</label>
-                <input type="text" name="password" onChange={changeHandler} value={user.password}/>
-            </div>
-            <button type="submit">submit</button>
-        </form>
-    );
-}
- 
+  const formik = useFormik({ initialValues: { name: "", email: "" ,password:""} });
+  console.log(formik.values);
+  
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("ok");
+  };
+  return (
+    <form onSubmit={submitHandler}>
+      <div>
+        <label>Name</label>
+        <input
+          type="text"
+          name="name"
+          onChange={formik.handleChange}
+          value={formik.values.name}
+        />
+      </div>
+      <div>
+        <label>Email</label>
+        <input
+          type="text"
+          name="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
+      </div>
+      <div>
+        <label>Password</label>
+        <input
+          type="text"
+          name="password"
+          onChange={formik.handleChange}
+          value={formik.values.password}
+        />
+      </div>
+      <button type="submit">submit</button>
+    </form>
+  );
+};
+
 export default SignUpForm;
